f9afe342ca4cac0109c4266ec38503cc56636626
acaValkyrie <acavalkyrie@gmail.com>
2024-03-20 23:46:32 +0900

    描画をスプライトに変更

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@
 #include "lgfx.h"
 #include "vector.h"
 
-LGFX display;
 Adafruit_BNO055 accel_sensor = Adafruit_BNO055(55, 0x28, &Wire);
+
+static LGFX display;
+static LGFX_Sprite sprite(&display);
 
 Vector3 accel;
 Vector3 velocity;
@@ -35,16 +37,16 @@ void setup(void) {
 
   velocity = Vector3(0, 0, 0);
   position = Vector3(0, 0, 0);
-  pos_max = Vector3(display.width() - r, display.height() - r, 10);
-  pos_min = Vector3(r, r, 0);
+  const int margin = r + 1;
+  pos_max = Vector3(display.width() - margin, display.height() - margin, 10);
+  pos_min = Vector3(margin, margin, 0);
   x_draw = position.x;
   y_draw = position.y;
+  sprite.createSprite(display.width(), display.height());
 }
 
 void loop(void) {
-  display.setCursor(0, 0);
   imu::Vector<3> accelermetor = accel_sensor.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-
   accel.x = accelermetor.x() * 0.1;
   accel.y = (accelermetor.y()*(0.7) + accelermetor.z()*(-0.7)) * 0.1;
   accel.z = (accelermetor.y()*(-0.7) + accelermetor.z()*(-0.7)) * 0.1;
@@ -54,40 +56,21 @@ void loop(void) {
   position.x += velocity.x;
   position.y += velocity.y;
   position.z += velocity.z;
+
   const float bounce = -0.5;
-  if (position.x > pos_max.x){
-    position.x = pos_max.x;
-    velocity.x *= bounce;
-  }
-  if (position.x < pos_min.x){
-    position.x = pos_min.x;
-    velocity.x *= bounce;
-  }
-  if (position.y > pos_max.y){
-    position.y = pos_max.y;
-    velocity.y *= bounce;
-  }
-  if (position.y < pos_min.y){
-    position.y = pos_min.y;
-    velocity.y *= bounce;
-  }
-  if (position.z > pos_max.z){
-    position.z = pos_max.z;
-    velocity.z *= bounce;
-  }
-  if (position.z < pos_min.z){
-    position.z = pos_min.z;
-    velocity.z *= bounce;
-  }
+  if (position.x > pos_max.x){ position.x = pos_max.x; velocity.x *= bounce; }
+  if (position.x < pos_min.x){ position.x = pos_min.x; velocity.x *= bounce; }
+  if (position.y > pos_max.y){ position.y = pos_max.y; velocity.y *= bounce; }
+  if (position.y < pos_min.y){ position.y = pos_min.y; velocity.y *= bounce; }
+  if (position.z > pos_max.z){ position.z = pos_max.z; velocity.z *= bounce; }
+  if (position.z < pos_min.z){ position.z = pos_min.z; velocity.z *= bounce; }
+
   const int x_before = x_draw;
   const int y_before = y_draw;
   x_draw = position.x;
   y_draw = position.y;
-  display.drawCircle(x_before, y_before, r, 0x0000);
-  display.drawCircle(x_draw, y_draw, r, 0xFFFF);
-  // display.printf("a x: %.2f \ty: %.2f \tz: %.2f\n", accel.x, accel.y, accel.z);
-  // display.printf("v x: %.2f \ty: %.2f \tz: %.2f\n", velocity.x, velocity.y, velocity.z);
-  // display.printf("p x: %.2f \ty: %.2f \tz: %.2f\n", position.x, position.y, position.z);
-
-  delay(10);
+  sprite.fillCircle(x_before, y_before, r, 0x0000);
+  sprite.fillCircle(x_draw, y_draw, r, 0xFFFF);
+  sprite.pushSprite(&display, 0, 0);
+  delay(1);
 }
